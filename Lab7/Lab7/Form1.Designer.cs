5297fdde99fa5b12e099411b1b48f76781d45157
Rafik <bikmaevrafik@gmail.com>
2022-03-09 12:01:05 +0300

    Lab7 - Solution

---
+++ Lab7/Lab7/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOpenKey = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOpenKey
+            // 
+            this.lblOpenKey.AutoSize = true;
+            this.lblOpenKey.Location = new System.Drawing.Point(139, 52);
+            this.lblOpenKey.Name = "lblOpenKey";
+            this.lblOpenKey.Size = new System.Drawing.Size(0, 20);
+            this.lblOpenKey.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Открытый ключ: ";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(14, 438);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(1238, 207);
+            this.txtResult.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Результат: ";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(1112, 351);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(140, 45);
+            this.btnEncrypt.TabIndex = 13;
+            this.btnEncrypt.Text = "Зашифровать";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(14, 351);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(140, 45);
+            this.btnDecrypt.TabIndex = 12;
+            this.btnDecrypt.Text = "Расшифровать";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(14, 108);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(1238, 237);
+            this.txtMsg.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Сообщение: ";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(14, 14);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(173, 35);
+            this.btnGenerate.TabIndex = 9;
+            this.btnGenerate.Text = "Сгенерировать";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 659);
+            this.Controls.Add(this.lblOpenKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblOpenKey;
+        private Label label3;
+        private TextBox txtResult;
+        private Label label2;
+        private Button btnEncrypt;
+        private Button btnDecrypt;
+        private TextBox txtMsg;
+        private Label label1;
+        private Button btnGenerate;
+    }
+}
---
+++ Lab7/Lab7/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        private const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя1234567890 ,./;:()!";
+
+        private List<BigInteger> _publicKey = new();
+        private BigInteger _privateKey;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            GenerateKeys();
+            lblOpenKey.Text = "(" + _publicKey.ElementAt(0) + ", " + _publicKey.ElementAt(1) + ", " + _publicKey.ElementAt(2) + ")";
+        }
+
+        private void GenerateKeys()
+        {
+            _publicKey.Clear();
+            _privateKey = 0;
+
+            var p = PrimeUtils.Generate(5);
+            var g = MathUtils.FindPrimitiveRoot(p - 1, p);
+            var x = p - 1;
+
+            while (!PrimeUtils.IsCoprime(x, p - 1))
+            {
+                x = new Random().Next(2, p - 2);
+            }
+
+            var y = BigInteger.ModPow(g, x, p);
+
+            _publicKey.Add(y);
+            _publicKey.Add(g);
+            _publicKey.Add(p);
+
+            _privateKey = x;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (_privateKey <= 0 || _publicKey?.Count <= 0)
+            {
+                MessageBox.Show("Ключ не сгенерирован", "Ошибка!", MessageBoxButtons.OK);
+                return;
+            }
+
+            txtResult.Text = Encrypt(txtMsg.Text);
+        }
+
+        private string Encrypt(string text)
+        {
+            var encryptionString = string.Empty;
+
+            var rng = RandomNumberGenerator.Create();
+            for (int i = 0; i < text.Length; i++)
+            {
+                var y = _publicKey.ElementAt(0);
+                var g = _publicKey.ElementAt(1);
+                var p = _publicKey.ElementAt(2);
+
+                var temp = ALPHABET.IndexOf(text[i]);
+
+                var k = p;
+
+                var randomNumber = new byte[1];
+
+                while (!PrimeUtils.IsCoprime(k, p - 1) || k < 2 || k > p - 2)
+                {
+                    rng.GetBytes(randomNumber);
+                    k = randomNumber[0];
+                }
+
+                var a = BigInteger.ModPow(g, k, p);
+                var b = (BigInteger.ModPow(y, k, p) * temp % p) % p;
+
+                encryptionString += a + "|" + b + " ";
+            }
+
+            return encryptionString;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (_privateKey <= 0 || _publicKey?.Count <= 0)
+            {
+                MessageBox.Show("Ключ не сгенерирован", "Ошибка!", MessageBoxButtons.OK);
+                return;
+            }
+
+            txtResult.Text = Decrypt(txtMsg.Text);
+        }
+
+        private string Decrypt(string text)
+        {
+            var decryptionString = string.Empty;
+            var encodedString = text.Split(' ');
+
+            for (int i = 0; i < encodedString.Length - 1; i++)
+            {
+                var encodedPairs = encodedString[i].Split('|');
+                var p = _publicKey.ElementAt(2);
+
+                BigInteger a = BigInteger.Parse(encodedPairs[0]);
+                BigInteger b = BigInteger.Parse(encodedPairs[1]);
+                BigInteger index = (b % p * BigInteger.ModPow(a, p - 1 - _privateKey, p)) % p;
+                decryptionString += ALPHABET[(int)index];
+            }
+
+            return decryptionString;
+        }
+    }
+}
